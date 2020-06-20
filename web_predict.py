e37a8b022010ca6be050acfae60a8e99cc57e0c5
Shadieq <32037920+catlaughing@users.noreply.github.com>
2020-06-21 02:13:56 +0700

    first push heroku

---
+++ web_predict.py
@@ -0,0 +1,15 @@
+from preprocessing import preprocessing_image, get_encoding
+from inference import create_caption
+from tensorflow.keras.models import load_model
+
+import warnings
+
+warnings.filterwarnings("ignore")
+model = load_model('models/model_512_6_20.h5')
+
+def
+caption
+
+
+
+
